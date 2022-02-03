0d81d02bbee8128505a7f1fc17c2ada651ef9880
Itzel Jiménez <itzeljimenez.do@gmail.com>
2022-02-03 04:32:40 -0600

    create BranchDetails componente and integrate service to retrieve branch commits

---
+++ src/views/Branches/BranchDetails.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+const BranchDetails = ({branchDetails}) => {    
+    const [rows, setRows] = useState([]);
+    const createData = (commit, message, author, timestamp) => {
+        return { commit, message, author, timestamp };
+    }
+
+    const getData = () => {
+        return branchDetails?.length > 0 ? branchDetails.map(element => {
+            return createData(element?.sha, element?.commit?.message, element?.commit?.author?.name, element?.commit?.author?.date)
+        }): [];
+    }
+
+    useEffect(()=> {
+        setRows(getData());
+    }, [])
+
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                    <TableCell>Commit </TableCell>
+                    <TableCell align="center">Message</TableCell>
+                    <TableCell align="right">Author</TableCell>
+                    <TableCell align="right">Timestamp</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows?.length > 0 ? rows.map((row) => (
+                    <TableRow
+                        key={row.commit}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                        <TableCell component="th" scope="row">
+                        {row.commit}
+                        </TableCell>
+                        <TableCell align="center">{row.message}</TableCell>
+                        <TableCell align="right">{row.author}</TableCell>
+                        <TableCell align="right">{row.timestamp}</TableCell>
+                    </TableRow>
+                    )): null}
+                </TableBody>
+            </Table>
+        </TableContainer>)
+}
+
+export default BranchDetails;
--- src/views/Branches/Branches.jsx
+++ src/views/Branches/Branches.jsx
@@ -1,11 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 import Layout from '../Layout/Layout';
 
-const Branches = () => (
-    <Layout layoutTitle="Branches">
-      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, autem corporis. Debitis, facere reprehenderit accusamus inventore architecto dolor animi tempore! Officiis sapiente, et sit debitis corrupti maiores quia laborum aliquid?</h1>
-    </Layout>
-);
+import { getBranchCommits, getBranchDetails, getBranches } from '../../services/git-services/gitServices';
+import BranchDetails from './BranchDetails';
+import { Divider } from '@mui/material';
+
+const Branches = () => {
+    const [expanded, setExpanded] = useState(false);
+    const [branches, setBranches] = useState();
+    const [branchDetails, setBranchDetails] = useState();
+
+    const handleChange = (name) => (event, isExpanded) => {
+        setExpanded(isExpanded ? name : false);
+        getBranchData(name)
+    };
+
+    const getBranchesResponse = async() => {
+        const branches = await getBranches();
+        setBranches(branches);
+    }
+
+    const getBranchData = async(branchName) => {
+        const branches = await getBranchCommits(branchName);
+        setBranchDetails(branches);
+    }
+
+    useEffect(()=> {
+        getBranchesResponse()
+    }, [])
+    
+    return (
+        <Layout layoutTitle="Branches">
+            <div>
+                <Accordion>
+                    <AccordionSummary
+                    id="panel1bh-header"
+                    >
+                        <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                            <b>BRANCH NAME</b>
+                        </Typography>
+                        <Typography sx={{ color: 'text.secondary' }}><b>DETAILS</b> </Typography>
+                    </AccordionSummary>
+                 
+                </Accordion>
+                {branches?.length > 0 ? branches.map(element => 
+                    <Accordion key={element.commit.sha} expanded={expanded === element.name} onChange={handleChange(element.name)}>
+                        <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1bh-content"
+                        id="panel1bh-header"
+                        >
+                            <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                               <b>{element.name}</b>
+                            </Typography>
+                            <Typography sx={{ color: 'text.secondary' }}><b>Commit:</b> {element.commit.sha}</Typography>
+                        </AccordionSummary>
+                        <Divider />
+                        <AccordionDetails>
+                            <BranchDetails branchDetails={branchDetails}/>
+                        </AccordionDetails>
+                    </Accordion>
+                ): null}
+               
+                
+            </div>
+        </Layout>
+    )
+};
 
 
 export default Branches;
